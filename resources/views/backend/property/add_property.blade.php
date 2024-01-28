c11c6248dfcc80ef70ec2a6c89d82c259f31e0f5
musfiqur521 <musfiqur521@gmail.com>
2024-01-28 10:19:03 +0600

    Show Imge Preview Using Js

--- resources/views/backend/property/add_property.blade.php
+++ resources/views/backend/property/add_property.blade.php
@@ -25,18 +25,53 @@
                     </div><!-- Col -->
                     <div class="col-sm-6">
                         <div class="mb-3">
-                            <label class="form-label">Last Name</label>
-                            <select class="form-select" id="exampleFormControlSelect1">
-                    <option selected="" disabled="">Select your age</option>
-                    <option>12-18</option>
-                    <option>18-22</option>
-                    <option>22-30</option>
-                    <option>30-60</option>
-                    <option>Above 60</option>
+                            <label class="form-label">Property Status</label>
+                            <select name="property_status" class="form-select" id="exampleFormControlSelect1">
+                    <option selected="" disabled="">Select Status</option>
+                    <option value="rent">For Rent</option>
+                    <option value="buy">For Buy</option>
                 </select>
                         </div>
                     </div><!-- Col -->
+
+                    <div class="col-sm-6">
+                        <div class="mb-3">
+                            <label class="form-label">Lowest Price</label>
+                            <input type="text" name="lowest_price" class="form-control">
+                        </div>
+                    </div><!-- Col -->
+
+                    <div class="col-sm-6">
+                        <div class="mb-3">
+                            <label class="form-label">Max Price</label>
+                            <input type="text" name="max_price" class="form-control">
+                        </div>
+                    </div><!-- Col -->
+
+                    <div class="col-sm-6">
+                        <div class="mb-3">
+                            <label class="form-label">Main Thambnail</label>
+                            <input type="file" name="property_thambnail" class="form-control" onchange="mainThamUrl(this)">
+
+                            <img src="" id="mainThamb">
+                        </div>
+                    </div><!-- Col -->
+
+                    <div class="col-sm-6">
+                        <div class="mb-3">
+                            <label class="form-label">Multiple Image</label>
+                            <input type="file" name="multi_img[]" id="multiImg" class="form-control" multiple="">
+
+                            <div class="row" id="preview_img"></div>
+
+                        </div>
+                    </div><!-- Col -->
+
                 </div><!-- Row -->
+
+
+
+
                 <div class="row">
                     <div class="col-sm-4">
                         <div class="mb-3">
@@ -118,5 +153,50 @@
             
         </script>
 
+        {{-- Image Preview --}}
+
+<script type="text/javascript">
+    function mainThamUrl(input){
+        if(input.files && input.files[0]){
+            var reader = new FileReader();
+            reader.onload = function(e){
+                $('#mainThamb').attr('src', e.target.result).width(80).height(80);
+            }
+            reader.readAsDataURL(input.files[0]);
+        }
+    }
+</script>
+
+{{-- Multiple Image Preview --}}
+
+<script> 
+ 
+    $(document).ready(function(){
+     $('#multiImg').on('change', function(){ //on file input change
+        if (window.File && window.FileReader && window.FileList && window.Blob) //check File API supported browser
+        {
+            var data = $(this)[0].files; //this file data
+             
+            $.each(data, function(index, file){ //loop though each file
+                if(/(\.|\/)(gif|jpe?g|png|webp)$/i.test(file.type)){ //check supported file type
+                    var fRead = new FileReader(); //new filereader
+                    fRead.onload = (function(file){ //trigger function on successful read
+                    return function(e) {
+                        var img = $('<img/>').addClass('thumb').attr('src', e.target.result) .width(100)
+                    .height(80); //create image element 
+                        $('#preview_img').append(img); //append image to output element
+                    };
+                    })(file);
+                    fRead.readAsDataURL(file); //URL representing the file's data.
+                }
+            });
+             
+        }else{
+            alert("Your browser doesn't support File API!"); //if File API is absent
+        }
+     });
+    });
+     
+    </script>
 
 @endsection
